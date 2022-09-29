f395defefec5dd09439672b642304b6dc6968d04
Patrick Kurth <p.kurth@posteo.de>
2022-09-29 09:54:14 +0200

    retain data while shifting

--- verilog/shift_register.v
+++ verilog/shift_register.v
@@ -19,14 +19,20 @@ module shift_register
     wire [`DATA_LEN - 1:0] cells_out;
     wire data_out;
     assign data_out = cells_out[0];
-
     wire data_in_internal;
-    assign data_in_internal = write ? data_in : cells_out[0];
 
-    // FIXME: reading data does not have to destroy the data
-    //        just feed it back in the first chain flip-flop
+    // FIXME: this is done to retain the data in the shift register
+    // however, the data_inout pin is driven with exactly the same data so
+    // that can be read back directly (without the mux). Check this!
+    mux data_in_mux (
+        .A(data_in),
+        .B(data_out),
+        .SEL(write),
+        .O(data_in_internal)
+    );
+
     register_cell regcell[`DATA_LEN - 1:0](
-        .chain_in({ data_in, cells_out[`DATA_LEN - 1:1] }),
+        .chain_in({ data_in_internal, cells_out[`DATA_LEN - 1:1] }),
         .update(update),
         .clk(clk),
         .reset(reset),
