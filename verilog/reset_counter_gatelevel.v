0f5806d4f9f114296905539f5005fc87a5bc60db
philippnickel <philipp.nickel@campus.tu-berlin.de>
2023-01-19 19:42:22 +0100

    added gatelevel of reset counter

---
+++ verilog/reset_counter_gatelevel.v
@@ -0,0 +1,48 @@
+module reset_counter(clk, data, reset);
+    input clk;
+    input data;
+    output reset;
+    wire [3:0] outp;
+    wire [3:0] outn;
+    wire [3:0] next;
+    wire [3:0] carry;
+    wire [3:0] net0;
+    wire [3:0] low;
+    wire [3:0] high;
+    wire [3:0] highmux;
+
+    tie_lo _01_ (.O(low[0]));
+    tie_lo _02_ (.O(low[1]));
+    tie_lo _03_ (.O(low[2]));
+    tie_lo _04_ (.O(low[3]));
+    tie_hi _05_ (.O(high[0]));
+    tie_hi _06_ (.O(high[1]));
+    tie_hi _07_ (.O(high[2]));
+    tie_hi _08_ (.O(high[3]));
+    tie_hi _09_ (.O(highmux[0]));
+    tie_hi _10_ (.O(highmux[1]));
+    tie_hi _11_ (.O(highmux[2]));
+    tie_hi _12_ (.O(highmux[3]));
+    dffpq _11_ (.CLK(clk), .D(next[0]), .Q(outp[0]));
+    dffpq _12_ (.CLK(clk), .D(next[1]), .Q(outp[1]));
+    dffpq _13_ (.CLK(clk), .D(next[2]), .Q(outp[2]));
+    dffpq _14_ (.CLK(clk), .D(next[3]), .Q(outp[3]));
+    dffnq _15_ (.CLK(clk), .D(outp[0]), .Q(outn[0]));
+    dffnq _16_ (.CLK(clk), .D(outp[1]), .Q(outn[1]));
+    dffnq _17_ (.CLK(clk), .D(outp[2]), .Q(outn[2]));
+    dffnq _18_ (.CLK(clk), .D(outp[3]), .Q(outn[3]));
+    xnor_gate _16_ (.A(low[0]), .B(outn[0]), .O(net0[0]));
+    xnor_gate _17_ (.A(carry[0]), .B(outn[1]), .O(net0[1]));
+    xnor_gate _18_ (.A(carry[1]), .B(outn[2]), .O(net0[2]));
+    xnor_gate _19_ (.A(carry[2]), .B(outn[3]), .O(net0[3]));
+    or_gate _19_ (.A(low[0]), .B(outn[0]), .O(carry[0]));
+    or_gate _20_ (.A(carry[0]), .B(outn[1]), .O(carry[1]));
+    or_gate _21_ (.A(carry[1]), .B(outn[2]), .O(carry[2]));
+    or_gate _22_ (.A(carry[2]), .B(outn[3]), .O(carry[3]));
+    mux _22_ (.IP(net0[0]), .IN(highmux[0]), .SEL(reset), .O(next[0]));
+    mux _23_ (.IP(net0[1]), .IN(highmux[1]), .SEL(reset), .O(next[1]));
+    mux _24_ (.IP(net0[2]), .IN(highmux[2]), .SEL(reset), .O(next[2]));
+    mux _25_ (.IP(net0[3]), .IN(highmux[3]), .SEL(reset), .O(next[3]));
+
+    assign reset = !outn[3];
+endmodule
