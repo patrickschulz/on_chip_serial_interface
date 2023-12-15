83a876b0a5ac0865e2ab0cf5ef0c2ed07c5a34ae
Patrick Kurth <p.kurth@posteo.de>
2023-12-15 15:05:30 +0100

    added example resetpattern generator

--- verilog/generate_example_resetpattern.lua
+++ verilog/generate_example_resetpattern.lua
@@ -3,7 +3,7 @@ local file = io.open("resetpattern.lua", "w")
 file:write("return {\n")
 
 for i = 1, 8 do
-    file:write(string.format("    %d,\n", 0))
+    file:write(string.format("    %d,\n", i % 2 == 0 and 1 or 0))
 end
 
 file:write("}")
