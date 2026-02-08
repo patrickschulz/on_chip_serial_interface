8e5248599c75a26c1eb29fd595c8b963191476e7
Patrick Kurth <p.kurth@posteo.de>
2026-02-08 19:52:01 +0100

    updated Makefile

--- verilog/generate_testbench.lua
+++ verilog/generate_testbench.lua
@@ -5,7 +5,8 @@ local util = require "util"
 local lines = util.make_lines_insert()
 
 local lower_limit = 0
-local upper_limit = 2 ^ settings.data_length - 1
+--local upper_limit = 2 ^ settings.data_length - 1
+local upper_limit = 128
 
 lines:add("//`define DEBUG_LEVEL")
 lines:add("`define LOWER_LIMIT %d", lower_limit)
